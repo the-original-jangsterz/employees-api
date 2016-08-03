4b94e55a8b7f85695f62b42ed4c8c1e880cd5ee9
Peter Jang <peter.x.jang@gmail.com>
2016-08-02 19:25:46 -0500

    Add create api action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,4 +8,14 @@ class EmployeesController < ApplicationController
     @employee = Employee.find_by(id: params[:id])
     render 'show.json.jbuilder'
   end
+
+  def create
+    @employee = Employee.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email]
+    )
+    @employee.save
+    render 'show.json.jbuilder'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/employees' => 'employees#index'
+  post '/employees' => 'employees#create'
   get '/employees/:id' => 'employees#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
