89beade36060cdf13407bb82a485faf8bf1cd7b6
Peter Jang <peter.x.jang@gmail.com>
2016-08-23 19:24:45 -0500

    Add api authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  before_action :restrict_access
+
+  private
+
+  def restrict_access
+    # check if the request has an API key as part of it...
+    authenticate_or_request_with_http_token do |api_key, options|
+      User.find_by(api_key: api_key, email: request.headers['X-User-Email'])
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803013444) do
+ActiveRecord::Schema.define(version: 20160823235713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20160803013444) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "email"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
